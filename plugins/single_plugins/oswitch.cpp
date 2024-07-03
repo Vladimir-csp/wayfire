6d82c8ede996e26f267e1cb77ce92a8195f2b1a6
Ilia Bozhinov <ammen99@gmail.com>
2024-07-03 12:18:22 +0200

    shuffle around header files

--- plugins/single_plugins/oswitch.cpp
+++ plugins/single_plugins/oswitch.cpp
@@ -47,7 +47,6 @@ class wayfire_oswitch : public wf::plugin_interface_t
     {
         auto current_output = wf::get_core().seat->get_active_output();
         auto view = wf::toplevel_cast(wf::get_active_view_for_output(current_output));
-        LOGI("Found view ", view);
         if (view)
         {
             move_view_to_output(view, target_output, true);
