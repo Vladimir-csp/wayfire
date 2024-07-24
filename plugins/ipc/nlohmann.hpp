78b47a941eaae9af6f33a8556652db289d92d3c7
Ilia Bozhinov <ammen99@gmail.com>
2024-07-24 09:58:58 +0200

    add nlohmann.hpp

---
+++ plugins/ipc/nlohmann.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#define JSON_NO_IO
+#define JSON_HAS_FILESYSTEM 0
+#define JSON_HAS_EXPERIMENTAL_FILESYSTEM 0
+#define JSON_USE_GLOBAL_UDLS 0
+#include <nlohmann/json.hpp>
--- plugins/pch/pch.hpp
+++ plugins/pch/pch.hpp
@@ -4,9 +4,9 @@
 #define JSON_USE_GLOBAL_UDLS 0
 
 #include <nlohmann/json.hpp>
-//#include "../ipc/nlohmann.hpp"
-//#include "../ipc/ipc-helpers.hpp"
-//#include "../ipc/ipc-activator.hpp"
+// #include "../ipc/nlohmann.hpp"
+// #include "../ipc/ipc-helpers.hpp"
+// #include "../ipc/ipc-activator.hpp"
 
 #include <wayland-server.h>
 #include <glm/glm.hpp>
@@ -22,15 +22,15 @@
 #include <cmath>
 #include <cstddef>
 //
-//#include "wayfire/view-transform.hpp"
-//#include "wayfire/core.hpp"
-//#include "wayfire/per-output-plugin.hpp"
-//#include "wayfire/view-transform.hpp"
-//#include "wayfire/scene.hpp"
-//#include "wayfire/workarea.hpp"
-//#include "wayfire/seat.hpp"
-//#include "wayfire/render-manager.hpp"
+// #include "wayfire/view-transform.hpp"
+// #include "wayfire/core.hpp"
+// #include "wayfire/per-output-plugin.hpp"
+// #include "wayfire/view-transform.hpp"
+// #include "wayfire/scene.hpp"
+// #include "wayfire/workarea.hpp"
+// #include "wayfire/seat.hpp"
+// #include "wayfire/render-manager.hpp"
 //
-//#include "wayfire/plugins/common/workspace-wall.hpp"
-//#include "wayfire/plugins/common/move-drag-interface.hpp"
-//#include "wayfire/plugins/common/input-grab.hpp"
+// #include "wayfire/plugins/common/workspace-wall.hpp"
+// #include "wayfire/plugins/common/move-drag-interface.hpp"
+// #include "wayfire/plugins/common/input-grab.hpp"
